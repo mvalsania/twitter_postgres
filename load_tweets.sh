d7e4d3c3cc26f93e9b179543b72b0da46ba206cd
mvalsania <mvalsania25@cmc.edu>
2025-04-10 14:39:15 -0700

    -

--- load_tweets.sh
+++ load_tweets.sh
@@ -15,6 +15,7 @@ done
 
 echo 'load denormalized'
 for file in $files; do
-    cat "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:21012 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    cat "$file" | sed 's/\\u0000//g' | iconv -f utf-8 -t utf-8 -c | psql postgresql://postgres:pass@localhost:21012 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+
 done
 
