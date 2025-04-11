b6ce872d392de43eb8a749844fa04434074d5bc7
mvalsania <mvalsania25@cmc.edu>
2025-04-10 22:32:41 -0700

    final

--- load_tweets.py
+++ load_tweets.py
@@ -289,7 +289,7 @@ def insert_tweet(connection,tweet):
             'favorite_count':tweet['favorite_count'],
             'quote_count':tweet['quote_count'],
             'withheld_copyright':remove_nulls(tweet['user']['withheld_copyright']) if 'withheld_copyright' in tweet['user'] else None,
-            'withheld_in_countries': remove_nulls(tweet['user'].get('withheld_in_countries', None))
+            'withheld_in_countries': remove_nulls(tweet['user'].get('withheld_in_countries', None)),
             'source':remove_nulls(tweet['source']),
             'text':remove_nulls(text),
             'country_code':remove_nulls(country_code),
